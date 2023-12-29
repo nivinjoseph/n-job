b8454711ddf7bd67f695ffbb2e32bfdf95521662
Shrey Mahendru <shreymahendru@gmail.com>
2023-12-29 17:33:30 +0700

    remove moment

--- test/schedule.test.ts
+++ test/schedule.test.ts
@@ -1,19 +1,31 @@
-import moment from "moment";
 import { Exception } from "@nivinjoseph/n-exception";
 import { Schedule } from "../src/schedule.js";
 import test, { describe } from "node:test";
 import assert from "node:assert";
+import { DateTime } from "luxon";
+import { given } from "@nivinjoseph/n-defensive";
 
 
 await describe("Schedule", async () =>
 {
+    function createDateTime(value: string): DateTime
+    {
+        given(value, "value").ensureHasValue().ensureIsString().ensure(t => t.matchesFormat("####-##-## ##:##"));
+        
+        const dateTime = DateTime.fromFormat(value, "yyyy-MM-dd HH:mm");
+        
+        given(value, "value").ensure(_ => dateTime.isValid, "invalid date");
+        
+        return dateTime;
+    }
+    
     await describe("Single config", async () =>
     {
         await test("given schedule has no config, when next is calculated with reference 2018-11-09 22:28, then the result should be 2018-11-09 22:29", () =>
         {
             const schedule = new Schedule();
-            const reference = moment("2018-11-09 22:28").valueOf();
-            const expected = moment("2018-11-09 22:29").valueOf();
+            const reference = createDateTime("2018-11-09 22:28").valueOf();
+            const expected = createDateTime("2018-11-09 22:29").valueOf();
             const next = schedule.calculateNext(reference);
             assert.strictEqual(next, expected);
         });
@@ -22,8 +34,8 @@ await describe("Schedule", async () =>
         {
             const schedule = new Schedule();
             schedule.setMinute(1);
-            const reference = moment("2018-11-09 22:28").valueOf();
-            const expected = moment("2018-11-09 23:01").valueOf();
+            const reference = createDateTime("2018-11-09 22:28").valueOf();
+            const expected = createDateTime("2018-11-09 23:01").valueOf();
             const next = schedule.calculateNext(reference);
             assert.strictEqual(next, expected);
         });
@@ -32,8 +44,8 @@ await describe("Schedule", async () =>
         {
             const schedule = new Schedule();
             schedule.setHour(23);
-            const reference = moment("2018-11-09 22:28").valueOf();
-            const expected = moment("2018-11-09 23:00").valueOf();
+            const reference = createDateTime("2018-11-09 22:28").valueOf();
+            const expected = createDateTime("2018-11-09 23:00").valueOf();
             const next = schedule.calculateNext(reference);
             assert.strictEqual(next, expected);
         });
@@ -43,8 +55,8 @@ await describe("Schedule", async () =>
         {
             const schedule = new Schedule();
             schedule.setHour(23);
-            const reference = moment("2018-11-09 23:50").valueOf();
-            const expected = moment("2018-11-09 23:51").valueOf();
+            const reference = createDateTime("2018-11-09 23:50").valueOf();
+            const expected = createDateTime("2018-11-09 23:51").valueOf();
             const next = schedule.calculateNext(reference);
             assert.strictEqual(next, expected);
         });
@@ -53,8 +65,8 @@ await describe("Schedule", async () =>
         {
             const schedule = new Schedule();
             schedule.setHour(23);
-            const reference = moment("2018-11-09 23:59").valueOf();
-            const expected = moment("2018-11-10 23:00").valueOf();
+            const reference = createDateTime("2018-11-09 23:59").valueOf();
+            const expected = createDateTime("2018-11-10 23:00").valueOf();
             const next = schedule.calculateNext(reference);
             assert.strictEqual(next, expected);
         });
@@ -63,8 +75,8 @@ await describe("Schedule", async () =>
         {
             const schedule = new Schedule();
             schedule.setDayOfWeek(5);
-            const reference = moment("2018-11-12 23:50").valueOf();
-            const expected = moment("2018-11-16 00:00").valueOf();
+            const reference = createDateTime("2018-11-12 23:50").valueOf();
+            const expected = createDateTime("2018-11-16 00:00").valueOf();
             const next = schedule.calculateNext(reference);
             assert.strictEqual(next, expected);
         });
@@ -73,8 +85,8 @@ await describe("Schedule", async () =>
         {
             const schedule = new Schedule();
             schedule.setDayOfWeek(1);
-            const reference = moment("2018-11-12 23:50").valueOf();
-            const expected = moment("2018-11-12 23:51").valueOf();
+            const reference = createDateTime("2018-11-12 23:50").valueOf();
+            const expected = createDateTime("2018-11-12 23:51").valueOf();
             const next = schedule.calculateNext(reference);
             assert.strictEqual(next, expected);
         });
@@ -83,8 +95,38 @@ await describe("Schedule", async () =>
         {
             const schedule = new Schedule();
             schedule.setDayOfWeek(1);
-            const reference = moment("2018-11-12 23:59").valueOf();
-            const expected = moment("2018-11-19 00:00").valueOf();
+            const reference = createDateTime("2018-11-12 23:59").valueOf();
+            const expected = createDateTime("2018-11-19 00:00").valueOf();
+            const next = schedule.calculateNext(reference);
+            assert.strictEqual(next, expected);
+        });
+        
+        await test("given schedule has config dayOfWeek = 7, when next is calculated with reference 2018-11-18 23:50, then result should be 2018-11-18 23:51", () =>
+        {
+            const schedule = new Schedule();
+            schedule.setDayOfWeek(7);
+            const reference = createDateTime("2018-11-18 23:50").valueOf();
+            const expected = createDateTime("2018-11-18 23:51").valueOf();
+            const next = schedule.calculateNext(reference);
+            assert.strictEqual(next, expected);
+        });
+
+        await test("given schedule has config dayOfWeek = 7, when next is calculated with reference 2018-11-18 23:59, then result should be 2018-11-25 00:00", () =>
+        {
+            const schedule = new Schedule();
+            schedule.setDayOfWeek(7);
+            const reference = createDateTime("2018-11-18 23:59").valueOf();
+            const expected = createDateTime("2018-11-25 00:00").valueOf();
+            const next = schedule.calculateNext(reference);
+            assert.strictEqual(next, expected);
+        });
+        
+        await test("given schedule has config dayOfWeek = 7, when next is calculated with reference 2018-11-15 23:59, then result should be 2018-11-18 00:00", () =>
+        {
+            const schedule = new Schedule();
+            schedule.setDayOfWeek(7);
+            const reference = createDateTime("2018-11-15 23:59").valueOf();
+            const expected = createDateTime("2018-11-18 00:00").valueOf();
             const next = schedule.calculateNext(reference);
             assert.strictEqual(next, expected);
         });
@@ -93,8 +135,8 @@ await describe("Schedule", async () =>
         {
             const schedule = new Schedule();
             schedule.setDayOfMonth(1);
-            const reference = moment("2018-11-12 23:50").valueOf();
-            const expected = moment("2018-12-01 00:00").valueOf();
+            const reference = createDateTime("2018-11-12 23:50").valueOf();
+            const expected = createDateTime("2018-12-01 00:00").valueOf();
             const next = schedule.calculateNext(reference);
             assert.strictEqual(next, expected);
         });
@@ -104,8 +146,8 @@ await describe("Schedule", async () =>
         {
             const schedule = new Schedule();
             schedule.setDayOfMonth(12);
-            const reference = moment("2018-11-12 23:50").valueOf();
-            const expected = moment("2018-11-12 23:51").valueOf();
+            const reference = createDateTime("2018-11-12 23:50").valueOf();
+            const expected = createDateTime("2018-11-12 23:51").valueOf();
             const next = schedule.calculateNext(reference);
             assert.strictEqual(next, expected);
         });
@@ -114,38 +156,38 @@ await describe("Schedule", async () =>
         {
             const schedule = new Schedule();
             schedule.setDayOfMonth(12);
-            const reference = moment("2018-11-12 23:59").valueOf();
-            const expected = moment("2018-12-12 00:00").valueOf();
+            const reference = createDateTime("2018-11-12 23:59").valueOf();
+            const expected = createDateTime("2018-12-12 00:00").valueOf();
             const next = schedule.calculateNext(reference);
             assert.strictEqual(next, expected);
         });
 
-        await test("given schedule has config month = 11, when next is calculated with reference 2018-11-12 23:59, then result should be 2018-12-01 00:00", () =>
+        await test("given schedule has config month = 11, when next is calculated with reference 2018-10-12 23:59, then result should be 2018-11-01 00:00", () =>
         {
             const schedule = new Schedule();
             schedule.setMonth(11);
-            const reference = moment("2018-11-12 23:59").valueOf();
-            const expected = moment("2018-12-01 00:00").valueOf();
+            const reference = createDateTime("2018-10-12 23:59").valueOf();
+            const expected = createDateTime("2018-11-01 00:00").valueOf();
             const next = schedule.calculateNext(reference);
             assert.strictEqual(next, expected);
         });
 
-        await test("given schedule has config month = 10, when next is calculated with reference 2018-11-12 23:59, then result should be 2018-11-12 24:00", () =>
+        await test("given schedule has config month = 10, when next is calculated with reference 2018-10-12 23:59, then result should be 2018-10-13 00:00", () =>
         {
             const schedule = new Schedule();
             schedule.setMonth(10);
-            const reference = moment("2018-11-12 23:59").valueOf();
-            const expected = moment("2018-11-12 24:00").valueOf();
+            const reference = createDateTime("2018-10-12 23:59").valueOf();
+            const expected = createDateTime("2018-10-13 00:00").valueOf();
             const next = schedule.calculateNext(reference);
             assert.strictEqual(next, expected);
         });
 
-        await test("given schedule has config month = 10, when next is calculated with reference 2018-11-30 23:59, then result should be 2019-11-01 00:00", () =>
+        await test("given schedule has config month = 11, when next is calculated with reference 2018-11-30 23:59, then result should be 2019-11-01 00:00", () =>
         {
             const schedule = new Schedule();
-            schedule.setMonth(10);
-            const reference = moment("2018-11-30 23:59").valueOf();
-            const expected = moment("2019-11-01 00:00").valueOf();
+            schedule.setMonth(11);
+            const reference = createDateTime("2018-11-30 23:59").valueOf();
+            const expected = createDateTime("2019-11-01 00:00").valueOf();
             const next = schedule.calculateNext(reference);
             assert.strictEqual(next, expected);
         });
@@ -158,8 +200,8 @@ await describe("Schedule", async () =>
             const schedule = new Schedule();
             schedule.setHour(12);
             schedule.setMinute(12);
-            const reference = moment("2018-11-30 01:59").valueOf();
-            const expected = moment("2018-11-30 12:12").valueOf();
+            const reference = createDateTime("2018-11-30 01:59").valueOf();
+            const expected = createDateTime("2018-11-30 12:12").valueOf();
             const next = schedule.calculateNext(reference);
             assert.strictEqual(next, expected);
         });
@@ -169,19 +211,19 @@ await describe("Schedule", async () =>
             const schedule = new Schedule();
             schedule.setHour(12);
             schedule.setMinute(12);
-            const reference = moment("2018-11-30 12:12").valueOf();
-            const expected = moment("2018-12-01 12:12").valueOf();
+            const reference = createDateTime("2018-11-30 12:12").valueOf();
+            const expected = createDateTime("2018-12-01 12:12").valueOf();
             const next = schedule.calculateNext(reference);
             assert.strictEqual(next, expected);
         });
 
-        await test("given schedule has config dayOfMonth = 12 month = 1, when next is calculated with reference 2018-11-30 12:12, then result should be 2019-02-12 00:00", () =>
+        await test("given schedule has config dayOfMonth = 12 month = 1, when next is calculated with reference 2018-11-30 12:12, then result should be 2019-01-12 00:00", () =>
         {
             const schedule = new Schedule();
             schedule.setDayOfMonth(12);
             schedule.setMonth(1);
-            const reference = moment("2018-11-30 12:12").valueOf();
-            const expected = moment("2019-02-12 00:00").valueOf();
+            const reference = createDateTime("2018-11-30 12:12").valueOf();
+            const expected = createDateTime("2019-01-12 00:00").valueOf();
             const next = schedule.calculateNext(reference);
             assert.strictEqual(next, expected);
         });
@@ -192,8 +234,8 @@ await describe("Schedule", async () =>
             schedule.setHour(12);
             schedule.setMinute(12);
             schedule.setDayOfMonth(14);
-            const reference = moment("2018-11-30 12:12").valueOf();
-            const expected = moment("2018-12-14 12:12").valueOf();
+            const reference = createDateTime("2018-11-30 12:12").valueOf();
+            const expected = createDateTime("2018-12-14 12:12").valueOf();
             const next = schedule.calculateNext(reference);
             assert.strictEqual(next, expected);
         });
@@ -204,67 +246,67 @@ await describe("Schedule", async () =>
             schedule.setHour(12);
             schedule.setMinute(12);
             schedule.setDayOfWeek(6);
-            const reference = moment("2018-11-13 12:12").valueOf();
-            const expected = moment("2018-11-17 12:12").valueOf();
+            const reference = createDateTime("2018-11-13 12:12").valueOf();
+            const expected = createDateTime("2018-11-17 12:12").valueOf();
             const next = schedule.calculateNext(reference);
             assert.strictEqual(next, expected);
         });
 
-        await test("given schedule has config month = 0 dayOfMonth = 6 hour = 12 min = 12, when next is calculated with reference 2018-11-13 12:12, then result should be 2019-01-06 12:12", () =>
+        await test("given schedule has config month = 1 dayOfMonth = 6 hour = 12 min = 12, when next is calculated with reference 2018-11-13 12:12, then result should be 2019-01-06 12:12", () =>
         {
             const schedule = new Schedule();
             schedule.setHour(12);
             schedule.setMinute(12);
             schedule.setDayOfMonth(6);
-            schedule.setMonth(0);
-            const reference = moment("2018-11-13 12:12").valueOf();
-            const expected = moment("2019-01-06 12:12").valueOf();
+            schedule.setMonth(1);
+            const reference = createDateTime("2018-11-13 12:12").valueOf();
+            const expected = createDateTime("2019-01-06 12:12").valueOf();
             const next = schedule.calculateNext(reference);
             assert.strictEqual(next, expected);
         });
 
-        await test("given schedule has config month = 0 dayOfMonth = 1 hour = 0 min = 0, when next is calculated with reference 2019-01-01 00:00, then result should be 2020-01-01 00:00", () =>
+        await test("given schedule has config month = 1 dayOfMonth = 1 hour = 0 min = 0, when next is calculated with reference 2019-01-01 00:00, then result should be 2020-01-01 00:00", () =>
         {
             const schedule = new Schedule();
             schedule.setHour(0);
             schedule.setMinute(0);
             schedule.setDayOfMonth(1);
-            schedule.setMonth(0);
-            const reference = moment("2019-01-01 00:00").valueOf();
-            const expected = moment("2020-01-01 00:00").valueOf();
+            schedule.setMonth(1);
+            const reference = createDateTime("2019-01-01 00:00").valueOf();
+            const expected = createDateTime("2020-01-01 00:00").valueOf();
             const next = schedule.calculateNext(reference);
             assert.strictEqual(next, expected);
         });
 
-        await test("given schedule has config month = 1 dayOfMonth = 31, when next is calculated with reference 2019-01-01 00:00, then InvalidScheduleException should be thrown", () =>
+        await test("given schedule has config month = 2 dayOfMonth = 31, when next is calculated with reference 2019-01-01 00:00, then InvalidScheduleException should be thrown", () =>
         {
             const schedule = new Schedule();
             schedule.setDayOfMonth(31);
-            schedule.setMonth(1);
-            const reference = moment("2019-01-01 00:00").valueOf();
+            schedule.setMonth(2);
+            const reference = createDateTime("2019-01-01 00:00").valueOf();
 
             assert.throws(() => schedule.calculateNext(reference),
                 (exp: Exception) => exp.name === "InvalidScheduleException");
         });
 
-        await test("given schedule has config month = 10 dayOfMonth = 31, when next is calculated with reference 2019-01-01 00:00, then InvalidScheduleException should thrown", () =>
+        await test("given schedule has config month = 11 dayOfMonth = 31, when next is calculated with reference 2019-01-01 00:00, then InvalidScheduleException should thrown", () =>
         {
             const schedule = new Schedule();
             schedule.setDayOfMonth(31);
-            schedule.setMonth(10);
-            const reference = moment("2019-01-01 00:00").valueOf();
+            schedule.setMonth(11);
+            const reference = createDateTime("2019-01-01 00:00").valueOf();
 
             assert.throws(() => schedule.calculateNext(reference),
                 (exp: Exception) => exp.name === "InvalidScheduleException");
         });
 
-        await test("given schedule has config month = 1 dayOfMonth = 29 hour, when next is calculated with reference 2018-01-01 00:00, then then result should be 2020-02-29 00:00", () =>
+        await test("given schedule has config month = 2 dayOfMonth = 29 hour, when next is calculated with reference 2018-01-01 00:00, then then result should be 2020-02-29 00:00", () =>
         {
             const schedule = new Schedule();
             schedule.setDayOfMonth(29);
-            schedule.setMonth(1);
-            const reference = moment("2018-01-01 00:00").valueOf();
-            const expected = moment("2020-02-29 00:00").valueOf();
+            schedule.setMonth(2);
+            const reference = createDateTime("2018-01-01 00:00").valueOf();
+            const expected = createDateTime("2020-02-29 00:00").valueOf();
             const next = schedule.calculateNext(reference);
             assert.strictEqual(next, expected);
         });
